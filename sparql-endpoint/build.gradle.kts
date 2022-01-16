053ee9697d73559b13ec099f96c1fb9341e4ebdd
subroh0508 <subroh.0508@gmail.com>
2022-01-17 05:57:09 +0900

    Add sparql-endpoint scripts

--- sparql-endpoint/build.gradle.kts
+++ sparql-endpoint/build.gradle.kts
@@ -1,19 +1,89 @@
-plugins {
-    java
+object Jena {
+    const val version = "4.3.2"
+    const val fuseki = "org.apache.jena:jena-fuseki-fulljar:$version"
+    const val commands = "org.apache.jena:jena-cmds:$version"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+object Dir {
+    const val jar = "jar"
+    const val lib = "lib"
+    const val bin = "bin"
+    const val dataset = "dataset/*"
+    const val tdb = "run/dataset"
+}
+
+val fuseki: Configuration by configurations.creating
+val commands: Configuration by configurations.creating
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    fuseki(Jena.fuseki)
+    commands(Jena.commands)
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+val clean by tasks.creating(Task::class) {
+    doLast {
+        delete {
+            delete(Dir.jar, Dir.lib, Dir.tdb)
+        }
+    }
+}
+
+val commandList = listOf("tdb1.xloader", "xload-common", "xload-main", "xload-data", "xload-index")
+val download by tasks.creating(Task::class) {
+    mustRunAfter(clean)
+    doLast {
+        copy {
+            from(fuseki)
+            into(Dir.jar)
+        }
+
+        copy {
+            from(commands)
+            into(Dir.lib)
+        }
+
+        File(Dir.bin).takeUnless { it.exists() }?.mkdirs()
+        commandList.forEach { name ->
+            exec {
+                executable("sh")
+                args("-c", "wget -P bin https://raw.githubusercontent.com/apache/jena/jena-${Jena.version}/apache-jena/bin/$name")
+            }
+        }
+
+        exec {
+            executable("sh")
+            args("-c", "chmod +x bin/*xload*")
+        }
+    }
+}
+
+val tdbLoad by tasks.creating(Task::class) {
+    mustRunAfter(clean, download)
+    doLast {
+        delete {
+            delete(Dir.tdb)
+        }
+
+        exec {
+            executable("sh")
+            args("-c", "bin/tdb1.xloader --loc ${Dir.tdb} ${Dir.dataset}")
+        }
+    }
+}
+
+val setup by tasks.creating(Task::class) {
+    dependsOn(clean, download, tdbLoad)
+}
+
+val start by tasks.creating(Task::class) {
+    doLast {
+        exec {
+            executable("sh")
+            args("-c", "java -jar jar/jena-fuseki-fulljar-4.3.2.jar --port=3000 --config=run/config.ttl")
+        }
+    }
 }
