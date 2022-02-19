b0aa7c85da94a96f78adf242dc75896f0ce8093c
subroh0508 <subroh.0508@gmail.com>
2022-02-19 20:31:34 +0900

    Add CodeMirrorTextArea.svelte and SparqlEditor.svelte

---
+++ frontend/src/lib/components/molecules/textarea/index.ts
@@ -0,0 +1,1 @@
+export { default as CodeMirrorTextArea } from './CodeMirrorTextArea.svelte';
---
+++ frontend/src/lib/components/organisms/editor/SparqlEditor.svelte
@@ -0,0 +1,54 @@
+<script context='module' lang='ts'>
+  
+</script>
+
+<script lang='ts'>
+  import { client } from '$lib/SparqlClient';
+
+  import Icon from 'svelte-awesome';
+  import { faPlay } from '@fortawesome/free-solid-svg-icons';
+  import { Button } from '$lib/components/atoms/button';
+  import { CodeMirrorTextArea } from '$lib/components/molecules/textarea';
+  import type { EditorFromTextArea } from 'codemirror';
+
+  export let query: string;
+  
+  let result = 'Show results here';
+  let editor: EditorFromTextArea = null;
+  let cursor = false;
+
+  function cursorMoved(event) {
+    cursor = true;
+    console.log('cursor activity');
+    // console.log(event.detail)
+  }
+
+  function onQueryChanged(event: CustomEvent) {
+    query = event.detail.value;
+
+    console.log('changed: ', query);
+  }
+
+  const handleClick = async () => {
+    const json = await client.execute(query);
+    result = JSON.stringify(json, null, 2);
+  }
+</script>
+
+<div>
+  <CodeMirrorTextArea
+    code={ query }
+    bind:editor={ editor }
+    on:activity={ cursorMoved }
+    on:change={ onQueryChanged }
+  />
+  <Button
+    label='Submit'
+    onClick={ handleClick }
+  >
+    <Icon data={ faPlay }/>
+  </Button>
+</div>
+<div>
+  <pre>{result}</pre>
+</div>
