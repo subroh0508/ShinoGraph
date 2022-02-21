3dd57c37facbe6c744f79e8da7436ea568e597e9
subroh0508 <subroh.0508@gmail.com>
2022-02-21 23:26:12 +0900

    Delete unused components

--- frontend/src/lib/editor/codemirror.js
+++
@@ -1,8 +0,0 @@
-import CodeMirror from 'codemirror';
-import 'codemirror/lib/codemirror.css';
-
-import 'codemirror/mode/sparql/sparql';
-import 'codemirror/addon/edit/matchbrackets';
-import 'codemirror/addon/edit/closebrackets';
-
-export default CodeMirror;
