0f37f4bd0e9c0b63c7a927775b02263fe8f86ae0
subroh0508 <subroh.0508@gmail.com>
2022-03-19 20:14:54 +0900

    Add Link and Meta

---
+++ frontend/src/components/atoms/link/Link.svelte
@@ -0,0 +1,13 @@
+<script lang='ts' context='module'>
+  export let href: string | null = null;
+</script>
+
+{#if !!href}
+  <a
+    href={ href }
+    target='_blank'
+    rel='noopener noreferrer'
+  ><slot/></a>
+{:else}
+  <slot/>
+{/if}
---
+++ frontend/src/components/atoms/text/Mata.svelte
@@ -0,0 +1,14 @@
+<script lang='ts' context='module'>
+  export let text: string | null = null;
+</script>
+
+{#if !!text}
+  <span class='meta'>{ text }</span>
+{/if}
+
+<style lang='scss'>
+  .meta {
+    font-size: 0.75rem;
+  }
+</style>
+
