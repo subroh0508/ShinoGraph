6d7a35e294a7133aeb2152927cac2661f38dbcb4
subroh0508 <subroh.0508@gmail.com>
2022-03-26 16:41:52 +0900

    Add @types directory

--- packages/@types/index.d.ts
+++ packages/@types/index.d.ts
@@ -1,4 +1,29 @@
-import { RDFMeta } from '$types/sparql';
+export interface QuerySolution {
+  [key: string]: RDF;
+}
+
+export interface Properties {
+  [key: string]: PropertyLabel;
+}
+
+export interface PropertyLabel {
+  default: string;
+  ja: string;
+}
+
+export interface RDF {
+  type: 'uri' | 'bnode' | 'literal';
+  value: string;
+  lang?: string;
+  datatype?: RDFMeta;
+}
+
+export interface RDFMeta {
+  href: string;
+  label: string;
+}
+
+export type NullableSolution = QuerySolution | null;
 
 export interface RDFEntity {
   subject: Subject;
